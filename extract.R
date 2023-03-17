e078c57770dfb436de742a7b090b4357fadc1e8c
pharmlovex <pharmifedayoojo@gmail.com>
2023-03-17 15:56:20 +0000

    Initial Commit

--- extract.R
+++ extract.R
@@ -1,4 +1,5 @@
 
+
 # Load Required Libraries  ------------------------------------------------
 
 library(GEOquery)
@@ -8,33 +9,25 @@ library(limma)
 library(affyPLM)
 
 
-# Create Function  --------------------------------------------------------
+# Extract Assay and Phenotype data -------------------------------------------
 
 extract_data <- function (accession_no, workdir){
 
                 workdir = workdir
                 accession_no = accession_no
-                # Fetch expression data
-                # download rawfile to the directory
+                setwd(workdir)
+                # Fetch rawfile to the directory
                 getGEOSuppFiles(accession_no)
                 # To open the tar file, access the folder with
                 setwd(paste0(workdir,"/",accession_no))
                 # Open the tar file
                 untar(paste0(accession_no,"_RAW.tar"))
-                # #Get a vector list of zippped GSM
-
-                Sys.sleep(120)
                 setwd(workdir)
+                # Get a vector list of zippped GSM
                 file_list<-list.files(accession_no, pattern  = ".CEL|.cel")
                 setwd(paste0(workdir,"/",accession_no))
-                # #Get a vector list of unzipped the CEL files
+                # Get a vector list of unzipped the CEL files
                 sapply(file_list, gunzip)
-                setwd(workdir)
-                # Get a list vector of the files from the folder
-                cel.list<-list.files(accession_no, pattern  = ".CEL|.cel",
-                                     full.names = F)
-
-                setwd(paste0(workdir,"/",accession_no))
 
                 # Read the CEl files into R, normalize with RMA and get the
                 #expression data set
@@ -42,6 +35,7 @@ extract_data <- function (accession_no, workdir){
                 exp.set=justRMA()
 
                 expr.data<-exprs(exp.set)
+                expr.df = as.data.frame(expr.data)
 
                 #Fetch phenotype data
 
@@ -50,8 +44,26 @@ extract_data <- function (accession_no, workdir){
                                       destdir = datadir)
                 pdata<- pdata.class[[1]]  %>%
                   pData()
-
-                extract_list <- list(expr.data, pdata)
+                # Gather the results together
+                extract_list <- list(expr.df, pdata)
+                names(extract_list) = c("assaydata", "phenotype")
+                setwd(workdir)
                 return(extract_list)
 
     }
+
+
+# Extract Assay  ----------------------------------------------------------
+
+
+extract.data <- extract_data(accession_no = "GSE158643",
+                         workdir = "C:/Users/dell/Downloads/DNAmicroarray")
+
+
+# Transform  --------------------------------------------------------------
+
+df = extract.data$assaydata
+transform_df <- transformMatrixCol(expMat = df,
+                                   Sep = "_",
+                                   nSep = 6,
+                                   pos = 1)
